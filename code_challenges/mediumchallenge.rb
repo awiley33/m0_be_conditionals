e314e59f6e87a8aa45ac538369d2ca0f80e9ae8f
Anna Wiley <awiley33@gmail.com>
2023-03-27 22:30:14 -0500

    Add solution to medium challenge

--- code_challenges/mediumchallenge.rb
+++ code_challenges/mediumchallenge.rb
@@ -13,3 +13,14 @@
 # should pay full price
 # - If the user is not over 25 and has a bad driving record, they need
 # to have someone else sign for the rental
+
+good_driving_record = false
+age = 24
+
+if good_driving_record == true && age >= 25
+    puts "Congrats! You get a discount on your car rental!"
+elsif good_driving_record == true || age >= 25
+    puts "Your car rental will be full price."
+elsif good_driving_record == false && age <25
+    puts "You will need another party to sign for your car rental."
+end
